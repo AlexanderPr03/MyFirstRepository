c6b8b8687f847f826dd411dd4235027504e4e92c
Alex <alexander.prohnitchi@gmail.com>
2023-04-18 19:53:18 +0300

    Removed a function from the script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-function fetchUsers() {
-    return fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())  
-           .then(users => users.map(user => user.name))
-           .catch((err) => notification.classList.add('visible'));       
-}
-
 async function fetchData() {
     const response = await fetch('https://jsdonplaceholder.typicode.com/users');
     const data = await response.json();
@@ -11,5 +5,6 @@ async function fetchData() {
     return data;
 }
 
-
+console.log(fetchData());
+console.log(fetchData());
 console.log(fetchData());
