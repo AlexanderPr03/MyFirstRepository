9e1786f7d759267462d929ee20310d97ce45ecb0
Alex <alexander.prohnitchi@gmail.com>
2023-04-18 19:56:54 +0300

    Revert "Removed a function from the script file"
    This reverts commit c6b8b8687f847f826dd411dd4235027504e4e92c.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+function fetchUsers() {
+    return fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())  
+           .then(users => users.map(user => user.name))
+           .catch((err) => notification.classList.add('visible'));       
+}
+
 async function fetchData() {
     const response = await fetch('https://jsdonplaceholder.typicode.com/users');
     const data = await response.json();
@@ -5,6 +11,5 @@ async function fetchData() {
     return data;
 }
 
-console.log(fetchData());
-console.log(fetchData());
+
 console.log(fetchData());
